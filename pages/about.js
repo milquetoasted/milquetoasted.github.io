e74db63978f137d3a98ba7b8a63b11e57ab3cd69
Lila Huang <moonhouse100@gmail.com>
2019-06-12 23:37:13 -0400

    pages, temporary loader

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+let about = async () => {
+  let src = `
+    <span>
+    <p>hi i'm lila and this site is very much under construction</p>
+    <p>i like coding, music, art, and being an armchair sports fan</p>
+    <p>here's my <a href="https://github.com/milquetoasted">github</a> / <a href="https://www.linkedin.com/in/lilahuang/">linkedin</a> / <a href="https://milquetoasted.tumblr.com/">tumblr</a> / <a href="https://jin-boyang.tumblr.com/">boyang jin fanblog</a> / resume</p>
+    <br>
+    <p class="contact">contact me:</p>
+    <form onsubmit="event.preventDefault()">
+    <p>
+    <span>name</span>
+    <input type="text"></input>
+    </p>
+    <p>
+    <span>email</span>
+    <input type="text"></input>
+    </p>
+    <p>
+    <span>message</span>
+    <textarea name="msg" rows="5"></textarea></p>
+    <button onclick="email(event)">submit</button>
+    </form>
+    </span>
+    `;
+  return src;
+}
+
+function email(e) {
+    e.preventDefault();
+    alert('boop');
+}
---
+++ pages/translations.js
@@ -0,0 +1,32 @@
+let translations = async () => {
+  let src = `
+  <div class="post">
+  <div class="imgs">
+  <img src="https://66.media.tumblr.com/2a31e98e6e3fb0ecb44fcf953c778265/tumblr_onv80c8pGE1tjxkoro1_540.gif">
+  <img src="https://66.media.tumblr.com/375fd02a3af8e59d5e44fad9a4d99a8e/tumblr_onv80c8pGE1tjxkoro2_540.gif">
+  <img src="https://66.media.tumblr.com/a719225f935fe89785b5867785a5fa72/tumblr_onv80c8pGE1tjxkoro3_540.gif">
+  <img src="https://66.media.tumblr.com/d81b824aa3b66ac78f4160b8c904cfec/tumblr_onv80c8pGE1tjxkoro4_540.gif">
+  </div>
+  <div class="posttitle">
+  <p><a href="http://sports.cctv.com/2017/03/28/VIDEECQ6hQtKKptipw7U2Qin170328.shtml"><b>Chinese pair skating - unlocking the dilemma: interviews with Hongbo Zhao, Sui/Han, Peng/Jin, and Yu/Zhang</b>.</a></p>
+  <p>Video published by CCTV5 on 28 Mar 2017, on the 风云会 program.</p>
+  <button>read more</button>
+  </div>
+  </div>
+
+  <div class="post">
+  <div class="posttitle">
+  <p><b><a href="http://xw.qq.com/sports/20170225012743/SPO2017022501274300?vt=4">Big People - Zijun Li: Growing and training alone, jumping until heaven falls and earth cracks.</a></b></p>
+  <p>Article &amp; video published on QQ Sports on 25 Feb 2017.</p>
+  <button>read more</button>
+  </div>
+  <div class="imgs">
+  <img src="https://66.media.tumblr.com/3ecbe1647c9a21d8f6d8fc5a441913d8/tumblr_on3cj0NwP51tjxkoro2_400.gif">
+  <img src="https://66.media.tumblr.com/bf3c28cf85b175167bce1091c061bf3c/tumblr_on3cj0NwP51tjxkoro4_400.gif">
+  <img src="https://66.media.tumblr.com/5979f86fb6a1e9462ea8273dfd81ff81/tumblr_on3cj0NwP51tjxkoro7_400.gif">
+  <img src="https://66.media.tumblr.com/6c22b342380449cf6eb8aa0a44717024/tumblr_on3cj0NwP51tjxkoro1_400.gif">
+  </div>
+  </div>
+  `;
+  return src;
+}
