b7d75695c0eb192a1373faf10ee4165d58bbd1aa
Lila <lila@Lilas-MacBook-Pro.local>
2021-01-04 20:38:17 -0500

    added some infographics

--- pages/infographics.js
+++ pages/infographics.js
@@ -2,27 +2,41 @@ let infographics =
 `
   <div class="post">
   <div class="imgs">
-  <img src="https://66.media.tumblr.com/2a31e98e6e3fb0ecb44fcf953c778265/tumblr_onv80c8pGE1tjxkoro1_540.gif">
-  <img src="https://66.media.tumblr.com/375fd02a3af8e59d5e44fad9a4d99a8e/tumblr_onv80c8pGE1tjxkoro2_540.gif">
-  <img src="https://66.media.tumblr.com/a719225f935fe89785b5867785a5fa72/tumblr_onv80c8pGE1tjxkoro3_540.gif">
-  <img src="https://66.media.tumblr.com/d81b824aa3b66ac78f4160b8c904cfec/tumblr_onv80c8pGE1tjxkoro4_540.gif">
+  <img src="harbin1.png">
+  <img src="harbin2.png">
   </div>
   <div class="posttitle">
-  <p><a href="http://sports.cctv.com/2017/03/28/VIDEECQ6hQtKKptipw7U2Qin170328.shtml"><b>Chinese pair skating - unlocking the dilemma: interviews with Hongbo Zhao, Sui/Han, Peng/Jin, and Yu/Zhang</b>.</a></p>
-  <p>Video published by CCTV5 on 28 Mar 2017.</p>
+  <p>01/2017</p>
+  <p><b><a href="#/infographics/harbin">
+  Figure Skaters of Harbin
+  </a></b></p>
   </div>
   </div>
 
   <div class="post">
   <div class="posttitle">
-  <p><b><a href="http://xw.qq.com/sports/20170225012743/SPO2017022501274300?vt=4">Big People - Zijun Li: Growing and training alone, jumping until heaven falls and earth cracks.</a></b></p>
-  <p>Article &amp; video published on QQ Sports on 25 Feb 2017.</p>
+  <p>01/2017</p>
+  <p><b><a href="#/infographics/chen17"> 
+  Nathan Chen, 2017 US Nationals
+  </a></b></p>
   </div>
   <div class="imgs">
-  <img src="https://66.media.tumblr.com/3ecbe1647c9a21d8f6d8fc5a441913d8/tumblr_on3cj0NwP51tjxkoro2_400.gif">
-  <img src="https://66.media.tumblr.com/bf3c28cf85b175167bce1091c061bf3c/tumblr_on3cj0NwP51tjxkoro4_400.gif">
-  <img src="https://66.media.tumblr.com/5979f86fb6a1e9462ea8273dfd81ff81/tumblr_on3cj0NwP51tjxkoro7_400.gif">
-  <img src="https://66.media.tumblr.com/6c22b342380449cf6eb8aa0a44717024/tumblr_on3cj0NwP51tjxkoro1_400.gif">
+  <img src="https://64.media.tumblr.com/ee948d6b0a02a4b69e0b84b48e14b6a5/tumblr_ok7wryg6OR1tjxkoro2_640.png">
+  <img src="https://64.media.tumblr.com/14adeec58b29aed9cddc21adb3ad6984/tumblr_ok7wryg6OR1tjxkoro3_640.png">
+  </div>
+  </div>
+
+  <div class="post">
+  <div class="imgs">
+  <img src="review1.png">
+  <img src="review2.png">
+  </div>
+  <div class="posttitle">
+  <p>12/2016</p>
+  <p><b><a href="#/infographics/2016"> 
+  2016 in Review
+  </a></b></p>
+  <p>For the Reckoner of MGCI.</p>
   </div>
   </div>
 `; 
---
+++ pages/infographicsPages.js
@@ -0,0 +1,33 @@
+let infographicsHarbin =
+`
+  <div class="infographic">
+  <img src="https://64.media.tumblr.com/304e3c0ed458117616950ceb2dc71a76/tumblr_oj82efTTib1tjxkoro1_r2_1280.png">
+  <img src="https://64.media.tumblr.com/c8c0a38856dea025bef5d0c94975b0b0/tumblr_oj82efTTib1tjxkoro3_r7_1280.png">
+  <img src="https://64.media.tumblr.com/8178814dbdfc3328b65d8cb15be6cd2f/tumblr_oj82efTTib1tjxkoro2_r5_1280.png">
+  <img src="https://64.media.tumblr.com/d11b0e3077c0915684dea31012a8e731/tumblr_oj82efTTib1tjxkoro5_r2_1280.png">
+  <img src-"https://64.media.tumblr.com/856eb405b9632a948807ba994bac1f79/tumblr_oj82efTTib1tjxkoro4_r2_1280.png">
+  <img src="https://64.media.tumblr.com/d021fa283a788aae156c609e3fb6bed2/tumblr_oj82efTTib1tjxkoro8_r3_1280.png">
+  <img src="https://64.media.tumblr.com/81453f4290a7e94ec0657bde8e03759b/tumblr_oj82efTTib1tjxkoro6_r2_1280.png">
+  <img src="https://64.media.tumblr.com/7d370e6ef7cc98d1ba90b3dbb3ede538/tumblr_oj82efTTib1tjxkoro7_r3_1280.png">
+  </div>
+  <div class='back'><a href="#/infographics">back to infographics</a></div>
+`; 
+
+let infographicsChen =
+`
+  <div class="infographic">
+  <img src="https://64.media.tumblr.com/31a3ef56b55b54ed69a1a77faae70843/tumblr_ok7wryg6OR1tjxkoro1_640.png">
+  <img src="https://64.media.tumblr.com/ee948d6b0a02a4b69e0b84b48e14b6a5/tumblr_ok7wryg6OR1tjxkoro2_640.png">
+  <img src="https://64.media.tumblr.com/14adeec58b29aed9cddc21adb3ad6984/tumblr_ok7wryg6OR1tjxkoro3_640.png">
+  <img src="https://64.media.tumblr.com/3ba495a94f973d10d2958fab59b08f41/tumblr_ok7wryg6OR1tjxkoro4_640.png">
+  </div>
+  <div class='back'><a href="#/infographics">back to infographics</a></div>
+`; 
+
+let infographics2016 =
+`
+  <div class="infographic">
+  <img src="2016 in review NO NUMBERS.png" style="max-width:700px;">
+  </div>
+  <div class='back'><a href="#/infographics">back to infographics</a></div>
+`; 
